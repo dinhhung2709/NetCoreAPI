99297add112b59aabb1842e1ab392fdd8fb4c8d3
“dinhhung2709” <dinhhung27092004@gmail.com>
2026-01-27 00:58:00 +0700

    CreatProject

--- FirstWebMVC/Controllers/DemoController.cs
+++ FirstWebMVC/Controllers/DemoController.cs
@@ -1,9 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
-
-namespace DemoMVC.Controllers
+using System.Text.Encodings.Web;
+namespace FirstWebMVC.Controllers
 {
     public class DemoController : Controller
-    {
+    { 
+        // GET: /HelloWorld/
         public IActionResult Index()
         {
             return View();
@@ -11,6 +12,12 @@ namespace DemoMVC.Controllers
         public IActionResult Demo()
         {
             return View();
+        }
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "Hello Nguyễn Đình Hưng 2221050384 ";
         }
     }
 }
